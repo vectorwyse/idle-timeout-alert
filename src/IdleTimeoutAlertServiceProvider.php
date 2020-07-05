ff02340b1ce655509bc85efb803cb6f8eb3124eb
Jeffrey Li <jeffrey@vectorwyse.com>
2020-07-05 19:23:35 -0400

    Initial commit

---
+++ src/IdleTimeoutAlertServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Vectorwyse\IdleTimeoutAlert;
+
+use Illuminate\Support\ServiceProvider;
+
+class IdleTimeoutAlertServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
